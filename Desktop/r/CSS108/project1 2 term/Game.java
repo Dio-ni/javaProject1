e5ae3938e6fd8b21890a413d1c3dbd68378567e4
Dayana Bolatbekova <200103264@stu.sdu.edu.kz>
2021-03-27 01:32:05 +0600

    Map without exception

---
+++ Desktop/r/CSS108/project1 2 term/Game.java
@@ -0,0 +1,151 @@
+import java.util.Scanner;
+import java.io.*;
+/*
+public class Game implements Player{
+   public static void main(String[] args){
+
+   }
+   Scanner input;
+   private Map map;
+   public Game(Map map){
+      this.map = map(input.nextInt());
+   }
+   public void setMap(Map newMap){
+
+   }
+   public void addPlayer(Player player){
+
+   }
+   public static void main(String[] args){
+      
+   }
+   @Override
+   public void moveRight() {
+      // TODO Auto-generated method stub
+      
+   }
+   @Override
+   public void moveLeft() {
+      // TODO Auto-generated method stub
+      
+   }
+   @Override
+   public void moveUp() {
+      // TODO Auto-generated method stub
+      
+   }
+   @Override
+   public void moveDown() {
+      // TODO Auto-generated method stub
+      
+   }
+   @Override
+   public Position getPosition() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+}
+
+class Map extends InvalidMapException{
+   private int size;
+   public Map(int size){
+      this.size = size;
+   }
+   public int getSize(){
+      return 0;
+   }
+   public char getValueAt(int a, int b){
+      return 'P';
+
+   }
+   public void print(){
+      
+   }
+}
+
+class InvalidMapException extends Exception{
+   String string;
+   public InvalidMapException(String string){
+      this.string = string;
+   }
+}
+
+interface Player{
+   void setMap(Map newMap);
+   void moveRight();
+   void moveLeft();
+   void moveUp();
+   void moveDown();
+   Position getPosition();
+
+}
+class MyPlayer extends Map implements Player{
+   public MyPlayer(int size) {
+      super(size);
+   }
+
+   private Map map;
+   Scanner input;
+   @Override
+   public void setMap(Map map) {
+      this.map = main(input.nextInt());
+   }
+
+   @Override
+   public void moveRight() {
+
+      
+   }
+
+   @Override
+   public void moveLeft() {
+      
+   }
+
+   @Override
+   public void moveUp() {
+      
+   }
+
+   @Override
+   public void moveDown() {
+      
+   }
+
+   @Override
+   public Position getPosition() {
+     Position position = new Position(0, 0);
+      return position;
+   }
+
+}
+
+class Position extends MyPlayer{
+   int x;
+   int y;
+   public Position(int x, int y){
+      this.x = x;
+      this.y = y;
+   }
+   public void setX(int x){
+      this.x =x;
+   }
+   public void setY(int y){
+      this.y =y;
+   }
+   public int getX(int x){
+      return this.x;
+   }
+   public int getY(){
+      return this.y;
+   }
+   public boolean equals(Position position){
+      return true;
+   }
+   public String toString(){
+      return "Some String";
+   }
+
+}
+*/
